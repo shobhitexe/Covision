fba0cbab0ca4f88f93b996d601a62957e0f70bbf
Shobhit <shobhitmir@gmail.com>
2021-07-10 03:29:48 +0530

    added sentiment analyzer

--- base/models.py
+++ base/models.py
@@ -11,6 +11,12 @@ from sklearn.metrics import r2_score
 import datetime
 import plotly.graph_objects as go
 from plotly.offline import download_plotlyjs,init_notebook_mode,plot,iplot
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+import neattext.functions as nf
+from textblob import TextBlob
+import plotly.express as px
 # Create your models here.
 
 class Predictor():
@@ -73,5 +79,97 @@ class Predictor():
         fig.add_trace(go.Scatter(x=dataset['Date_reported'],y=dataset['New_cases'],name='Prehistoric Data'))
         fig.add_trace(go.Scatter(x=result['Date_reported'],y=result['New_cases'],name='Forecasted Data'))
         fig.update_layout(title='Daily Cases VS Date',xaxis_title='Date',yaxis_title='Number of Daily Cases')
+        plot_div=plot(fig, output_type='div',include_plotlyjs=False)
+        return plot_div
+
+tweet_dataset = pd.DataFrame()
+
+class Listener(StreamListener):
+    def __init__(self,filename):
+        super(Listener,self).__init__()
+        self.counter = 0
+        self.filename = filename
+
+    def on_status(self, status):
+        global tweet_dataset
+        if self.counter < 10:
+            try:
+                if hasattr(status, 'retweeted_status') and hasattr(status.retweeted_status, 'extended_tweet'):
+                    tweet_dataset = tweet_dataset.append({'tweet':status.retweeted_status.extended_tweet['full_text']},ignore_index=True)
+                    self.counter+=1
+                if hasattr(status, 'extended_tweet'):
+                    tweet_dataset = tweet_dataset.append({'tweet':status.extended_tweet['full_text']},ignore_index=True)
+                    self.counter+=1
+                else:
+                    pass
+            except AttributeError:
+                pass
+        else:
+            return False
+
+    def on_error(self,status):
+        if status==420:
+            print(status)
+            return False
+        
+class TwitterStreamer():
+    API_KEY = '46bd35ovqNJBO5ny5xSu0IY97'
+    API_SECRET_KEY = 'm62qpyHEcFRagq1H37SdkwH8IQFZgCXYjXmKAFku2NdcIXF4vz'
+    ACCESS_TOKEN = '1413146033991733265-LqS7YwKXgRQoeDYQyNxgE0atJ52onx'
+    ACCESS_TOKEN_SECRET = 'CU1e7KlTIfbzkKtrCjzsmcwh1LqyclNPH9AVq5XFZtCY7'
+
+    def stream_tweets(self,filename,hashtags):
+        listener = Listener(filename)
+        auth = OAuthHandler(self.API_KEY,self.API_SECRET_KEY)
+        auth.set_access_token(self.ACCESS_TOKEN,self.ACCESS_TOKEN_SECRET)
+        stream = Stream(auth,listener,tweet_mode='extended')
+        stream.filter(track=hashtags,languages=['en'])
+
+
+class SentimentAnalyzer():
+    def get_tweets(self):
+        hashtags = ['covid','covid19','COVID19','CORONAVIRUS','coronavirus','COVID']
+        filename = 'tweets.json'
+        twitter_streamer = TwitterStreamer()
+        twitter_streamer.stream_tweets(filename,hashtags)
+        global tweet_dataset
+        return tweet_dataset
+
+    def clean_tweets(self,dataset):
+        dataset['tweet'] = dataset['tweet'].apply(nf.remove_hashtags)
+        dataset['tweet'] = dataset['tweet'].apply(lambda x: nf.remove_userhandles(x))
+        dataset['tweet'] = dataset['tweet'].apply(nf.remove_multiple_spaces)
+        dataset['tweet'] = dataset['tweet'].apply(nf.remove_urls)
+        dataset['tweet'] = dataset['tweet'].apply(nf.remove_puncts)
+        dataset['tweet'] = dataset['tweet'].apply(nf.remove_emojis)
+        dataset['tweet'] = dataset['tweet'].apply(lambda x: x.replace('RT ',''))
+        return dataset
+
+    def get_sentiment(self,dataset):
+        sentiment = []
+        polarity = []
+        for tweet in dataset['tweet']:
+            analysis = TextBlob(tweet)
+            if analysis.sentiment.polarity>0:
+                senti = 'Positive'
+            elif analysis.sentiment.polarity==0:
+                senti = 'Neutral'
+            else:
+                senti = 'Negative'
+            sentiment = np.append(sentiment,senti)
+            polarity = np.append(polarity,analysis.sentiment.polarity)
+        dataset['polarity'] = polarity
+        dataset['sentiment'] = sentiment 
+        return dataset
+    
+    def visualize(self,dataset):
+        neg = dataset[dataset['sentiment']=='Negative']['sentiment'].count()
+        pos = dataset[dataset['sentiment']=='Positive']['sentiment'].count()
+        neu = dataset[dataset['sentiment']=='Neutral']['sentiment'].count()
+        freq = {'Positive':pos,'Negative':neg,'Neutral':neu}
+        X = list(freq.keys())
+        Y = list(freq.values())
+        fig = px.bar(x=X, y=Y)
+        fig.update_layout(title='Count of Positive/Negative/Neutral Tweets Related To Covid-19',xaxis_title='Predicted Sentiment',yaxis_title='Count')
         plot_div=plot(fig, output_type='div',include_plotlyjs=False)
         return plot_div
--- base/views.py
+++ base/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-from .models import Predictor
+from .models import Predictor,SentimentAnalyzer
 import json
+import pandas as pd
 # Create your views here.
 
 def home_view(request):
@@ -24,7 +25,19 @@ def predict_view(request):
         return render(request,'predict.html',context=ctx)
     return render(request,'predict.html')
 
+@csrf_exempt
 def sentiment_view(request):
+    if request.method=='POST':
+        analyzer = SentimentAnalyzer()
+        tweets = analyzer.get_tweets()
+        tweets = analyzer.clean_tweets(tweets)
+        result = analyzer.get_sentiment(tweets)
+        freq_plot_div = analyzer.visualize(result)
+        result_json = result.reset_index().to_json(orient ='records') 
+        result = [] 
+        result = json.loads(result_json)
+        ctx = {'result':result,'freq_plot_div':freq_plot_div}
+        return render(request,'sentiment.html',context=ctx)
     return render(request,'sentiment.html')
 
 def summary_view(request):
