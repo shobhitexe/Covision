1a96db50234caa69364c497b42253fe149e9c907
Shobhit <shobhitmir@gmail.com>
2021-07-10 00:53:17 +0530

    added covid trend predictor

--- base/models.py
+++ base/models.py
@@ -1,3 +1,77 @@
 from django.db import models
-
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from django.contrib.staticfiles import finders
+from covision.settings import BASE_DIR
+import os
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+import datetime
+import plotly.graph_objects as go
+from plotly.offline import download_plotlyjs,init_notebook_mode,plot,iplot
 # Create your models here.
+
+class Predictor():
+    def load_data(self):
+        csv_path = os.path.join(BASE_DIR,'base','datasets','covid_cases.csv')
+        dataset = pd.read_csv(csv_path)
+        dataset = dataset[['Date_reported','New_cases']]
+        return dataset
+
+    def predict(self,dataset):
+        X_dataset_train = dataset[0:int(len(dataset)*0.75)]
+        X_dataset_test = dataset[int(len(dataset)*0.75):]
+        training=pd.DataFrame(X_dataset_train['New_cases'])
+        testing=pd.DataFrame(X_dataset_test['New_cases'])
+        sc = StandardScaler()
+        training = sc.fit_transform(training)
+        testing = sc.transform(testing)
+        window=30
+        x_train=[]
+        y_train=[]
+        for i in range(window,len(training)):
+            x_train.append(training[i-window:i,0])  
+            y_train.append(training[i,0])
+        x_train,y_train=np.array(x_train),np.array(y_train)
+        regressor = LinearRegression()
+        regressor.fit(x_train, y_train)
+        x_test=[]
+        for i in range(window,len(testing)):
+            x_test.append(testing[i-window:i,0])  
+        x_test=np.array(x_test)
+        y_pred = sc.inverse_transform(regressor.predict(x_test))
+        Final = X_dataset_test[window:]
+        Final['Predicted'] = y_pred
+        X_dataset_test['Date_reported']=pd.to_datetime(X_dataset_test['Date_reported'],format="%Y-%m-%d")
+        for i in range (window):
+            x_test=[]
+            x_test.append(testing[-window:,0])  
+            x_test=np.array(x_test)
+            pred=sc.inverse_transform(regressor.predict(x_test))
+            new_date=X_dataset_test['Date_reported'][-1:] + datetime.timedelta(days=1)
+            data = {'Date_reported':new_date.values[0],'New_cases':pred[0]}
+            X_dataset_test = X_dataset_test.append(data,ignore_index=True)
+            testing = np.reshape(np.append(testing,regressor.predict(x_test)),(-1,1))
+        X_dataset_test['New_cases'][-window:] = X_dataset_test['New_cases'][-window:].apply(lambda x: int(round(x)))
+        return Final,X_dataset_test[-window:]
+
+    def get_accuracy(self,result):
+        return round(r2_score(result['New_cases'], result['Predicted'])*100,2)
+    
+    def visualize_test(self,Final):
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=Final['Date_reported'],y=Final['New_cases'],name='Actual'))
+        fig.add_trace(go.Scatter(x=Final['Date_reported'],y=Final['Predicted'],name='Predicted'))
+        fig.update_layout(title='Daily Cases VS Date',xaxis_title='Date',yaxis_title='Number of Daily Cases')
+        plot_div=plot(fig, output_type='div',include_plotlyjs=False)
+        return plot_div
+
+    def visualize_forecast(self,dataset,result):
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=dataset['Date_reported'],y=dataset['New_cases'],name='Prehistoric Data'))
+        fig.add_trace(go.Scatter(x=result['Date_reported'],y=result['New_cases'],name='Forecasted Data'))
+        fig.update_layout(title='Daily Cases VS Date',xaxis_title='Date',yaxis_title='Number of Daily Cases')
+        plot_div=plot(fig, output_type='div',include_plotlyjs=False)
+        return plot_div
--- base/views.py
+++ base/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
-
+from django.views.decorators.csrf import csrf_exempt
+from .models import Predictor
+import json
 # Create your views here.
 
 def home_view(request):
     return render(request,'home.html')
 
+@csrf_exempt
 def predict_view(request):
+    if request.method=='POST':
+        predictor = Predictor()
+        dataset = predictor.load_data()
+        test_result,forecast_result = predictor.predict(dataset)
+        accuracy = predictor.get_accuracy(test_result)
+        test_plot_div = predictor.visualize_test(test_result)
+        forecast_plot_div = predictor.visualize_forecast(dataset,forecast_result)
+        forecast_result.Date_reported=forecast_result.Date_reported.dt.strftime('%Y-%m-%d')
+        forecasted_json = forecast_result.reset_index().to_json(orient ='records') 
+        forecast_result = [] 
+        forecast_result = json.loads(forecasted_json) 
+        ctx = {'accuracy':accuracy,'dataset':dataset,'forecast_result':forecast_result,'test_div':test_plot_div,'forecast_div':forecast_plot_div}
+        return render(request,'predict.html',context=ctx)
     return render(request,'predict.html')
 
 def sentiment_view(request):
