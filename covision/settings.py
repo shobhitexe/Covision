9c4edd7054b3af7273b2ecdedc3631c214de65ba
Shobhit <shobhitmir@gmail.com>
2021-12-12 20:16:28 +0530

    changed static settings

--- covision/settings.py
+++ covision/settings.py
@@ -122,7 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATIC_URL = '/static/'
 
 # Default primary key field type
